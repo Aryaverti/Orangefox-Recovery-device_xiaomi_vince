f50e6d7b893d548b564274652cbe25f85c398510
Aryaverti <tg967591@gmail.com>
2026-02-03 21:38:42 +0530

    Add full_base.mk inheritance to twrp_vince.mk

--- twrp_vince.mk
+++ twrp_vince.mk
@@ -17,8 +17,9 @@
 #
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/otter/config/common.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
 # Inherit Telephony packages
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
