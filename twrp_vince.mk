b38c75c74d056fd09b0fb3b0baa8268fa1d71c57
Aryaverti <tg967591@gmail.com>
2026-02-03 21:16:39 +0530

    Update twrp_vince.mk

--- twrp_vince.mk
+++ twrp_vince.mk
@@ -16,11 +16,8 @@
 # limitations under the License.
 #
 
-# Specify phone tech before including full_phone
-$(call inherit-product, vendor/omni/config/gsm.mk)
-
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/otter/config/common.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
 # Inherit Telephony packages
